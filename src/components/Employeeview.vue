f1f3097c5143afa73ae778fdd62f978a021d472d
najeeb39 <96305189+najeeb39@users.noreply.github.com>
2023-02-08 21:46:32 +0530

    configured component with router

--- src/components/Employeeview.vue
+++ src/components/Employeeview.vue
@@ -1,1 +1,17 @@
-
+<template>
+    <div class="hello">
+      <h1>{{ msg }}</h1>
+      <h2>Employee View</h2>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    name: 'EmployeeView',
+    data () {
+      return {
+        msg: 'Welcome to Your Vue.js App'
+      }
+    }
+  }
+  </script>
